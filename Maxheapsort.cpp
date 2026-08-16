b9e1a08701c53768c5219a5ca07d7860efd0224d
GopiRamesh19 <damarla.ramesh129466@marwadiuniversity.ac.in>
2026-08-16 22:37:37 +0530

    Maxheapsort.cpp

--- Maxheapsort.cpp
+++ Maxheapsort.cpp
@@ -19,11 +19,8 @@ void heapify(int a[], int n, int i) {
 }
 
 void heapSort(int a[], int n) {
-    // Build Max Heap
     for (int i = n / 2 - 1; i >= 0; i--)
         heapify(a, n, i);
-
-    // Extract elements from heap
     for (int i = n - 1; i > 0; i--) {
         swap(a[0], a[i]);
         heapify(a, i, 0);
